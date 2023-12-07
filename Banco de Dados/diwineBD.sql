378d0469d6fc66b51dc9cc54aea10e6bdc3596cd
giuliacarmona <giulia.ayres@sptech.school>
2023-12-07 15:38:14 -0300

    Update no banco de Dados e section HelpDesk

--- Banco de Dados/diwineBD.sql
+++ Banco de Dados/diwineBD.sql
@@ -16,79 +16,68 @@ INSERT INTO empresa VALUES										-- Inserindo algumas empresas (ficticio)
 	(NULL, 'Empresa D', '46.375.009/0001-39');
 
 -- *** CRIANDO TABELA 'FUNCIONARIO E INSERINDO DADOS' ***--
-CREATE TABLE funcionario (
-idFunc			INT PRIMARY KEY AUTO_INCREMENT,					-- Identificação do funcionário Responsável
-nomeFunc		VARCHAR(60),									-- Nome Completo
-CPF				VARCHAR(14) NOT NULL,							-- CPF
-cargo			VARCHAR(40),									-- Cargo que ele ocupa na empresa
-email			VARCHAR(40) NOT NULL,							-- E-mail cadastro e utilizado para realizar o login
+CREATE TABLE usuario (
+idUsuario		INT PRIMARY KEY AUTO_INCREMENT,					-- Identificação do funcionário Responsável
+nome			VARCHAR(60),									-- Nome Completo
+email			VARCHAR(40) NOT NULL UNIQUE,					-- E-mail cadastro e utilizado para realizar o login
+senha 			VARCHAR(20),									-- Senha cadastro e utilizado para realizar o login
 celular			VARCHAR(14),									-- Telefone para contato
 fkEmpresa		INT,											
-	CONSTRAINT fkEmpresa FOREIGN KEY (fkEmpresa)
-		REFERENCES empresa(idEmpresa)
-)AUTO_INCREMENT = 100; 
+	FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa)
+) AUTO_INCREMENT = 100; 
 
-INSERT INTO funcionario VALUES									-- Inserindo um funcionário
-	(NULL, 'José Silva', '555.555.555-55', 'Técnico de Manutenção', 'jose.silva@empresab.com', '(11)94002-8922', 1);
+INSERT INTO usuario VALUES										-- Inserindo um funcionário
+	(NULL, 'José Silva', 'jose.silva@empresab.com', 'joseEmpresa22@', '11962063620',1);
 
 -- *** CRIANDO TABELA 'AMBIENTE' ***--
 create table ambiente (
 idAmbiente		INT PRIMARY KEY AUTO_INCREMENT,					-- Identificação do Ambiente
 fkEmpresa		INT,
-	CONSTRAINT fkEmpresaAmbiente FOREIGN KEY (fkEmpresa)
-		REFERENCES empresa(idEmpresa)
+	FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa)
 );
 
 -- *** INSERINDO DUAS COLUNA NA TABELA 'ambiente' ***--
-ALTER TABLE ambiente ADD COLUMN nomeAmbiente VARCHAR(40);		-- Nome que o usuário deseja dar ao ambiente
-ALTER TABLE ambiente ADD COLUMN alertaMensagem VARCHAR(100);	-- Mensagem de Alerta caso a temperatura ou umidade ultrapsse o limite máximo
+ALTER TABLE ambiente ADD COLUMN apelido VARCHAR(40);			-- Nome que o usuário deseja dar ao ambiente
 DESC ambiente;
 
 -- *** INSERINDO DADOS NA TABELA 'AMBIENTE' ***--
 INSERT INTO ambiente VALUES
-	(NULL, 1, 'Setor 1 - Tinto', '0'),
-    (NULL, 1, 'Setor 2 - Branco', '0'),
-    (NULL, 2, 'Setor 3 - Rosé', '0');
-
+	(NULL, 1, 'Setor China - Tinto'),
+    (NULL, 1, 'Setor Coreia - Branco'),
+    (NULL, 1, 'Setor Japão - Rosé');
 
 -- *** CRIANDO TABELA 'LOTE' ***--
 create table lote (
 idLote			INT PRIMARY KEY AUTO_INCREMENT,	 				-- Identificação do Lote
-tipoVinho		VARCHAR(40) NOT NULL,							-- Branco, Tinto, Rosé
-tempoMaturacao 	CHAR(2),										-- 5, 10, 20 anos
+apelido			VARCHAR(100),									-- Apelido do Lote
+tipoVinho		VARCHAR(40),							-- Branco, Tinto, Rosé
 dataIncio		DATE,											-- EX: 03-05-2018
+tempoMes	 	INT,											
+tempoAno		INT,
+quantidade		INT,											-- Quantidade de Lotes
 fkAmbiente		INT,
-	CONSTRAINT fkAmbienteLote FOREIGN KEY (fkAmbiente)
-		REFERENCES ambiente(idAmbiente)
-);
-   
--- *** INSERINDO UMA COLUNA NA TABELA 'LOTE' ***--
-ALTER TABLE lote ADD COLUMN tipoBarril VARCHAR(40) NOT NULL; 	-- Barril de Carvalho, Ovo de Concreto ou Tanque de Aço
+	FOREIGN KEY (fkAmbiente) REFERENCES ambiente(idAmbiente)
+) AUTO_INCREMENT = 1;
+
 DESC lote;
 
 -- *** INSERINDO DADOS NA TABELA 'LOTE' ***--
 INSERT INTO lote VALUES
-	(NULL, 'tinto', '10', '2018-03-05', 3, 'Barril de Carvalho');
+	(NULL, 'Barril Coreia - Tinto', 'tinto', '2018-03-05', 3, 2020, 20, 1);
 
 -- *** CRIANDO TABELA 'SENSOR' E INSERINDO DADOS ***--
 CREATE TABLE sensor (
-idSensor 		INT PRIMARY KEY AUTO_INCREMENT,					-- Identificação do Ambiente
-tipoSensor	 	VARCHAR(100),									-- Umidade e Temperatura
-atualizacao		VARCHAR(40),									-- A cada 10, 15 ou 30 Minutos
-dtRegistro		DATE,											-- Data do registro
-localizacao		VARCHAR(30),									-- Localização do Sensor
-fkAmbiente		INT,
-	CONSTRAINT fkAmbienteSensor FOREIGN KEY (fkAmbiente)
-		REFERENCES ambiente(idAmbiente)
+idSensor 			INT PRIMARY KEY AUTO_INCREMENT,					-- Identificação do Ambiente
+dht11_umidade 		DECIMAL,										-- Umidade DHT11
+dht11_temperatura 	DECIMAL,										-- Temperatura DHT11
+momento 			DATETIME,
+fkAmbiente			INT,
+	FOREIGN KEY (fkAmbiente) REFERENCES ambiente(idAmbiente)
 );
-
-INSERT INTO Sensor VALUES										
-	(NULL, 'Temperatura', 15, '2019-12-12', 'Noroeste', 1),				-- Inserindo dados de Temperatura
-	(NULL, 'Umidade', 30, '2021-12-12', 'Sul', 1);					-- Inserindo dados de Umidade
   
 -- *** SELECT * FROM DAS TABELAS ***--
 SELECT * FROM empresa;
-SELECT * FROM funcionario;
+SELECT * FROM usuario;
 SELECT * FROM ambiente;
 SELECT * FROM lote;
 SELECT * FROM sensor;
